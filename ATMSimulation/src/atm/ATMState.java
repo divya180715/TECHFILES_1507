0dd29e27a7713364062b17fc4799309b9776dd6d
Divyaparthasarathy <divyapems@gmail.com>
2026-04-19 17:49:00 +0530

    Delete ATMSimulation directory

--- ATMSimulation/src/atm/ATMState.java
+++
@@ -1,9 +0,0 @@
-package atm;
-
-public interface ATMState {
-    void insertCard(String cardNumber);
-    void enterBiometric(String code);
-    void enterPin(int pin);
-    void requestTransaction(String type, double amount);
-    void ejectCard();
-}
